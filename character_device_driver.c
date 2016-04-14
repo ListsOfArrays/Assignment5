53a23e3cadea31f326d39873f8567c4d814c9011
ListsOfArrays <ListsOfArrays@users.noreply.github.com>
2016-04-14 18:48:57 -0400

    Added queuing functions, function prototypes in. All that's left is to fill everything in.

--- character_device_driver.c
+++ character_device_driver.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>	//Always needed for kernels.
 #include <linux/kernel.h>	//Needed for KERN_INFO
 #include <linux/init.h>		//Gives us __init & __exit
+#include <linux/fs.h>		//Gives us file_operations
+#include <asm/uaccess.h>		//Gives us put_user
 #define DRIVER_AUTHOR "Jacob [Cassagnol & Crandal]"
 #define DRIVER_DESC   "Assignment 5 character device driver"
 
@@ -11,12 +13,34 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 
-/// @brief Can't use free - that's in cstdlib.
+// Prototypes.
+// 'static' keeps everything internally linked.
+static int char_driver_init(void);
+static ssize_t char_driver_read(struct file *, char *, size_t, loff_t *);
+static ssize_t char_driver_write(struct file *, const char *, size_t, loff_t *);
+static int char_driver_open(struct inode *, struct file *);
+static int char_driver_release(struct inode *, struct file *);
+
+static int size_queue(void);
+static int insert_queue(char* vals, int vals_len);
+static int remove_queue(char* vals, int vals_len);
+
+// Our queue, statically allocated.
 static char queue[MAX_MEMORY] = {0};
 static int  front = 0;
 static int  back = 0;
+// Keep track of number of times device is open to allow for correct cleanup.
+static int deviceUser = 0;
 
-// static keeps everything inside this file.
+/// @brief file_operations links our functions to the system calls.
+static struct file_operations fops = {
+	.read = char_driver_read,
+	.write = char_driver_write,
+	.open = char_driver_open,
+	.release = char_driver_release,
+};
+
+/// @brief Initialization component for the character driver.
 static int __init char_driver_init (void) {
 	printk(KERN_INFO "Installing module.\n");
 	queue[0] = 'a';
@@ -24,8 +48,101 @@ static int __init char_driver_init (void) {
 	return 0;
 }
 
+/// @brief When the character driver is unloaded, this gets called.
 static void __exit char_driver_exit (void) {
 	printk(KERN_INFO "Removing module.\n");
+
+	// TODO: Fix this. Should be check_put or something like that.
+	if (deviceUser != 0)
+	{
+		printk(KERN_INFO "ERROR: Already have an open connection with char driver. FAILED.\n");
+	}
+}
+
+/// @brief Returns the current size of the queue.
+static int size_queue(void)
+{
+	if (back < front)
+		return back + MAX_MEMORY - front;
+	return back - front;
+}
+
+/// @brief   Safely inserts items from vals into the queue.
+/// @returns Returns count of bytes read into queue.
+static int insert_queue(char* vals, int vals_len)
+{
+	int bytes_read = 0;
+
+	for ( ; bytes_read < vals_len; bytes_read++)
+	{
+		if ((back + 1) % MAX_MEMORY == front)
+			break;
+
+		queue[back++] = vals[bytes_read];
+
+		if (back >= MAX_MEMORY)
+			back = 0;
+	}
+
+	return bytes_read;
+}
+
+/// @brief Safely removes items from queue and places them in vals.
+/// @returns Returns count of bytes removed from queue, or negative value for error.
+static int remove_queue(char* vals, int vals_len)
+{
+	int bytes_read = 0, errorVal = 0;
+	for ( ; bytes_read < vals_len; bytes_read++)
+	{
+		if (front == back)
+			break;
+
+		// Error condtion is non-zero return value.
+		errorVal = put_user(queue[front], &vals[bytes_read]);
+		if (errorVal != 0)
+			return errorVal > 0 ? -errorVal : errorVal;
+
+		front++;
+		if (front >= MAX_MEMORY)
+			front = 0;
+	}
+
+	return bytes_read;
+}
+
+/// @brief
+static ssize_t char_driver_read(struct file * filp, char * bufIn, size_t lenIn, loff_t * offset)
+{
+	printk(KERN_INFO "Reading char driver.\n");
+	return 0;
+}
+
+/// @brief
+static ssize_t char_driver_write(struct file * filp, const char * bufOut, size_t lenOut, loff_t * offset)
+{
+	printk(KERN_INFO "Writing char driver.\n");
+	return 0;
+}
+
+/// @brief
+static int char_driver_open(struct inode * inNode, struct file * filp)
+{
+	printk(KERN_INFO "Opening char driver.\n");
+	if (deviceUser == 1)
+	{
+		printk(KERN_INFO "ERROR: Already have an open connection with char driver. FAILED.\n");
+		return -EBUSY;
+	}
+	deviceUser = 1;
+	return 0;
+}
+
+/// @brief
+static int char_driver_release(struct inode * inNode, struct file * filp)
+{
+	printk(KERN_INFO "Releasing char driver.\n");
+	deviceUser = 0;
+	return 0;
 }
 
 module_init(char_driver_init);
